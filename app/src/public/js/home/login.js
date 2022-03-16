456908e884877e9be1acdf6d62f89941b9b9315c
YDG <zzmn1234@naver.com>
2022-03-16 23:08:15 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,7 +9,15 @@ loginBtn.addEventListener("click", login);
 function login() {
     const req = {
       id: id.value,
-      pasword: psword.value,
+      psword: psword.value,
     };
- console.log(req);
+ 
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  })
 }
