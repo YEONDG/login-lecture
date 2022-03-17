e9e5645ed534a057aea2fbf5a696997085e0a600
YDG <zzmn1234@naver.com>
2022-03-17 20:03:32 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const id = document.querySelector("#id");
+const name = document.querySelector("#name");
+const psword = document.querySelector("#psword");
+const confirmPsword = document.querySelector("#confirm-psword");
+const registerBtn = document.querySelector("#button");
+
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+    const req = {
+      id: id.value,
+      name: name.value,
+      psword: psword.value,
+      confirmPsword: confirmPsword.value,
+    };
+    console.log(req);
+ 
+
+//   fetch("/register", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(req)
+//   })
+//    .then((res) => res.json())
+//    .then((res) => {
+//      if (res.success) {
+//        location.href = "/";
+//      } else {
+//        alert(res.msg);
+//      }
+//    })
+//    .catch((err) =>{
+//      console.error(new Error("로그인 중 에러발생"));
+//    });
+}
